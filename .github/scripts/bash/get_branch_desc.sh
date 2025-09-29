032eae586dd178eee99fcb2bcdc593479dc08a5d
GeorgesTatchum <georgestatchum363@gmail.com>
2025-09-29 11:53:05 +0200

    updating the identification of a branch description

--- .github/scripts/bash/get_branch_desc.sh
+++ .github/scripts/bash/get_branch_desc.sh
@@ -3,14 +3,13 @@
 echo "# Branches du projet" > BRANCHES.md
 echo "" >> BRANCHES.md
 
-for branch in $(git branch -r | grep -v '\->'); do
+for branch in $(git branch -r | grep -v '->'); do
     branch_name=$(echo $branch | sed 's#origin/##')
-    desc=$(git log origin/$branch_name --grep="^desc:" --pretty=format:"%s" -1)
+    desc=$(git log origin/$branch_name --grep="^desc-$branch_name:" --pretty=format:"%s" -1)
     if [ -z "$desc" ]; then
         desc="(Pas de description)"
     else
-        desc=$(echo $desc | sed 's/^desc: //')
+        desc=$(echo $desc | sed "s/^desc-$branch_name: //")
     fi
     echo "- **$branch_name** : $desc" >> BRANCHES.md
 done
-
